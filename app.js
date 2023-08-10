39bf63f4cbf0dcd7a988383511268232f362c868
pcbman43 <kougia.mihkel@gmail.com>
2023-08-10 13:56:16 +0300

    Variables/Muutujad

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World!") 
+// variables
+// let
+let name = 'Anna Karutina'
+name = 'New name'
+
+// const
+const surname = 'Karutina'
+// surname = 'Karutin' - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Anna',
+    surname: 'Karutina'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = 'IKT'
+console.log(person)
