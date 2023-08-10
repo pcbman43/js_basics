0d82212845ca7e54f7b5bf4f05d4cf512f36bdcb
pcbman43 <kougia.mihkel@gmail.com>
2023-08-10 14:20:39 +0300

    Datatypes/Andmetüübid

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-// variables
-// let
+// data types
+// String
 let name = 'Anna Karutina'
-name = 'New name'
+console.log(name)
+console.log(typeof name)
 
-// const
-const surname = 'Karutina'
-// surname = 'Karutin' - error
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(percent)
+console.log(typeof percent)
+
+let isHere = true // false
+console.log(isHere)
+console.log(typeof isHere)
 
 // arrays
 const numbers = [1, 2, 3, 4, 5]
 console.log(numbers)
-numbers.push(6)
-console.log(numbers)
+console.log(typeof numbers)
 
 // objects
 const person = {
@@ -19,7 +27,19 @@ const person = {
     surname: 'Karutina'
 }
 console.log(person)
-console.log(person.firstname)
-console.log(person.surname)
-person.department = 'IKT'
-console.log(person)
+console.log(typeof person)
+
+// Null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+// Symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+// undefined
+let test
+console.log(test)
+console.log(typeof test)
