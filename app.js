36944cbb1b2550d1ace95f766da893f202150817
pcbman43 <kougia.mihkel@gmail.com>
2023-08-11 21:52:57 +0300

    Date processing/Ajatöötlus

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-const person = {
-    firstname: 'Kadi',
-    surname: 'Tamm',
-    age: 40,
-    email: 'kadi.tamm@gmail.com',
-    hobbies: ['tennis', 'korvpall'],
-    address: {
-        city: 'Võru',
-        county: 'Võrumaa'
-    },
-    getBirthYear: function (){
-        return 2022 - this.age
-    }
-}
+const today = new Date()
 
 let val
-val = person
-val = person.firstname
-val = person['surname']
-val = person["hobbies"][1]
-val = person.hobbies[0]
-val = person.address.city
-val = person.address['city']
-val = person.getBirthYear()
+
+val = today
+val = today.getMonth() // 0 - january
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getTime()
+
+val = new Date("09/22/2021")
+val = new Date('September 23 2022')
+
+val = new Date()
+val.setDate(23)
+val.setFullYear(2023)
+
 console.log(val)
-
-/*for(let i = 0; i < person.hobbies.length; i++){
-   console.log(person.hobbies[i])
-}*/
-
-person.hobbies.forEach((hobbie, index) => {
-   console.log(hobbie + " on element indeksiga " + index)
-})
