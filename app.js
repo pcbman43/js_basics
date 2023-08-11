137ddfaf2fdc86a97a6bab49b85c489f7504c872
pcbman43 <kougia.mihkel@gmail.com>
2023-08-11 22:32:06 +0300

    Functions/Funktsioonid

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-console.log('for loop')
-for (let number = 0; number < 10; number++) {
-    if(number == 5){
-        // break
-        continue
+/*
+function greeting(name){
+    // console.log(`Hello, ${name}!`)
+    return `Hello, ${name}!`
+}
+// greeting('Anna')
+// let greet = greeting('Anna')
+// console.log(greet)
+const square = function(number) {
+    return number * number
+}
+// let result = square(3)
+// console.log(result)
+*/
+
+// IIFE - Immediately Invokable Function Expression
+(function (){
+    console.log('function is running')
+})();
+
+(function (name){
+    console.log(`Hello, ${name}!`)
+})('Anna')
+
+const todo = {
+    add: function (){
+        console.log('add todo...')
+    },
+    edit: function (){
+        console.log('edit todo...')
+    },
+    delete: function (){
+        console.log('delete todo...')
     }
-    console.log(number)
 }
 
-console.log('while loop')
-let number1 = 11;
-while (number1 < 10){
-    console.log(number1)
-    number1++
-}
-
-console.log('do-while loop')
-let number2 = 11;
-do {
-    console.log(number2)
-    number2++
-} while (number2 < 10)
-
-console.log('for-each loop')
-const cars = ['Audi', 'Tesla', 'BMW']
-cars.forEach((car) => {
-    console.log(car)
-})
-
-const person = {
-    firstname: 'Kadi',
-    lastname: 'Tamm',
-    age: 40
-}
-
-console.log('for-in loop')
-for (let personKey in person) {
-    console.log(person[personKey])
-}
+todo.add()
+todo.edit()
+todo.delete()
